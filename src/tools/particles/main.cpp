45fe176d02da2cc4c0ff47490f084daea72ee9f9
Timothy Stark <fsword007@gmail.com>
2021-06-10 10:43:02 -0400

    Build stars database program added

---
+++ src/tools/particles/main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <vector>
+
+int main(int argc, char **argv)
+{
+    std::cout << "Building particle database" << std::endl;
+
+}
